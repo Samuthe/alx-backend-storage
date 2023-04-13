79e605bca5e24c9d21f918c8cf8205e651ed5c30
Samuthe <ekatisamuel@gmail.com>
2023-04-13 14:29:12 +0300

    create trigger

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -7,7 +7,9 @@
 -- You should use attributes formed and split for computing the lifespan
 -- Your script can be executed on any database
 
-SELECT band_name, (IFNULL(split, 2022) - formed)
+SELECT band_name, (IFNULL(
+    split, 2022) - formed)
+
 AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
 
 -- SELECT band_name, IFNULL(split, 2020) - formed as lifespan FROM metal_bands
