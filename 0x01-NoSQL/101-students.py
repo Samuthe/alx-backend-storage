20707ff5ef577d4d062c64758221e4dc7467d819
Samuthe <ekatisamuel@gmail.com>
2023-04-24 16:29:30 +0300

    returns std sorted by avg score

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,23 @@
+//Write a Python function that returns all students sorted by average score:
+
+//Prototype: def top_students(mongo_collection):
+//mongo_collection will be the pymongo collection object
+//The top must be ordered
+//The average score must be part of each item returns with key = averageScore
+
+#!/usr/bin/env python3
+"""
+Aggregation operations: average
+"""
+from collections import OrderedDict
+
+
+def top_students(mongo_collection):
+    """
+    Gets list of students from mongo collection
+    and returns the list of computed average for
+    each student
+    """
+    pipeline = [{'$addFields': {'averageScore': {'$avg': '$topics.score'}}},
+                {'$sort': OrderedDict([('averageScore', -1), ('name', 1)])}]
+    return mongo_collection.aggregate(pipeline)
