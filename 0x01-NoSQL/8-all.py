bdd0cd111e6fcd71c1f4a1fbc6161cc8d2417078
Samuthe <ekatisamuel@gmail.com>
2023-04-25 15:00:56 +0300

    list all

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,8 @@
 # Write a Python function that lists all documents in a collection:
-
-#     Prototype: def list_all(mongo_collection):
-#     Return an empty list if no document in the collection
-#     mongo_collection will be the pymongo collection object
-
+#!/usr/bin/env python3
+'''
+function to list all docs in mongo
+'''
 import pymongo
 
 
@@ -16,5 +15,3 @@ def list_all(mongo_collection):
         return [values for values in details]
     else:
         return []
-    
-    
